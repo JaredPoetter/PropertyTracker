ee337e635d2dbe5e0ccf379c89ba00fca1be0173
Jared Poetter <poet0007@umn.edu>
2014-10-12 13:10:58 -0500

    added in another tab for the settings view

---
+++ PropertyTracker/SettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.m
+//  PropertyTracker
+//
+//  Created by Jared Poetter on 10/12/14.
+//  Copyright (c) 2014 JP. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@implementation SettingsViewController
+
+@end
