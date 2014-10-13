4aad856474b3addab77c91344de0d24f06c80f21
Jared Poetter <poet0007@umn.edu>
2014-10-13 17:41:40 -0500

    added the add tenant view controller

---
+++ PropertyTracker/AddTenantViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddTenantViewController.h
+//  PropertyTracker
+//
+//  Created by Jared Poetter on 10/13/14.
+//  Copyright (c) 2014 JP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTenantViewController : UIViewController
+
+@end
---
+++ PropertyTracker/AddTenantViewController.m
@@ -0,0 +1,13 @@
+//
+//  AddTenantViewController.m
+//  PropertyTracker
+//
+//  Created by Jared Poetter on 10/13/14.
+//  Copyright (c) 2014 JP. All rights reserved.
+//
+
+#import "AddTenantViewController.h"
+
+@implementation AddTenantViewController
+
+@end
